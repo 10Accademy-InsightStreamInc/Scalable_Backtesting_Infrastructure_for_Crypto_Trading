566ff0bc2c109b714b117b9cb3cbcde1b6dc02cb
dev-abuke <abubekershamil123@gmail.com>
2024-06-17 20:55:47 +0300

    feat:  Multiple Backtests using different SMA

--- scripts/backtest.py
+++ scripts/backtest.py
@@ -1,11 +1,13 @@
 import backtrader as bt
 import pandas as pd
-import os
-import sys
 
-class TestStrategy(bt.Strategy):
+class SMAStrategy(bt.Strategy):
+    params = (
+        ('period', 15),
+    )
+
     def __init__(self):
-        self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=15)
+        self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.period)
 
     def next(self):
         if self.sma > self.data.close:
@@ -14,11 +16,14 @@ class TestStrategy(bt.Strategy):
             self.sell()
 
 if __name__ == '__main__':
-    print(os.getcwd())
     cerebro = bt.Cerebro()
     df = pd.read_csv('data/binance_btc_usdt_candlestick.csv', index_col='timestamp', parse_dates=True)
     data = bt.feeds.PandasData(dataname=df)
     cerebro.adddata(data)
-    cerebro.addstrategy(TestStrategy)
-    cerebro.run()
-    cerebro.plot()
+
+    # Run backtests with different SMA periods
+    for period in [10, 15, 20]:
+        cerebro.addstrategy(SMAStrategy, period=period)
+        print(f'Running backtest with SMA period: {period}')
+        cerebro.run()
+        cerebro.plot()

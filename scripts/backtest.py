e5dce724e0e7eb27f68794a95a44c58a47df7a58
dev-abuke <abubekershamil123@gmail.com>
2024-06-19 01:55:11 +0300

    feat: add multi backtesting

--- scripts/backtest.py
+++ scripts/backtest.py
@@ -8,12 +8,40 @@ class SMAStrategy(bt.Strategy):
 
     def __init__(self):
         self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.period)
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
 
     def next(self):
+        if self.order:
+            return
+
         if self.sma > self.data.close:
-            self.buy()
+            self.order = self.buy()
         elif self.sma < self.data.close:
-            self.sell()
+            self.order = self.sell()
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                self.sellprice = order.executed.price
+                self.sellcomm = order.executed.comm
+
+            self.bar_executed = len(self)
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        print(f'Operation Profit, Gross {trade.pnl}, Net {trade.pnlcomm}')
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
@@ -21,9 +49,9 @@ if __name__ == '__main__':
     data = bt.feeds.PandasData(dataname=df)
     cerebro.adddata(data)
 
-    # Run backtests with different SMA periods
+    metrics = []
+
     for period in [10, 15, 20]:
         cerebro.addstrategy(SMAStrategy, period=period)
         print(f'Running backtest with SMA period: {period}')
-        cerebro.run()
-        cerebro.plot()
+       

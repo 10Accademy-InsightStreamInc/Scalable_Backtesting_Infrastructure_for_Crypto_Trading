e6a76a5b2ed4cbb31f63f48770fc2709a9b08e1b
dev-abuke <abubekershamil123@gmail.com>
2024-06-20 12:42:05 +0300

    feat: create scene and backtest sql schema

---
+++ db/schema/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE scenes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    period INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE backtests (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    scene_id INT,
+    gross_profit FLOAT,
+    net_profit FLOAT,
+    number_of_trades INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (scene_id) REFERENCES scenes(id)
+);

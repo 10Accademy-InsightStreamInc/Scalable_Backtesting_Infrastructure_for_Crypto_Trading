7e57f54a26523b38bfa285112c21114b56d49ce9
dev-abuke <abubekershamil123@gmail.com>
2024-06-21 11:03:20 +0300

    feat: add database connection to backend

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://admin:<password>@<your-instance-endpoint>:5432/crypto_trading')
+DATABASE_URL = os.getenv('DATABASE_URL', "postgresql+psycopg2://group_three:lbRFEbA2bf3g5x6iDRrIsNqLycCznpab@dpg-cp6uiansc6pc73cnh6f0-a.oregon-postgres.render.com/amharic_contents")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

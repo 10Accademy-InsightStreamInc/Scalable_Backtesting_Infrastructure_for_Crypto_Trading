0bbf00f88c3b60db0a45c563581c1ed9bb50a501
dev-abuke <abubekershamil123@gmail.com>
2024-06-22 15:28:49 +0300

    refactor db

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("AWS_DATABASE_URL", "postgresql+psycopg2://group3:group3%40week9@g3.10academy.org/backtest")
+DATABASE_URL = os.getenv("PYCOPG_DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

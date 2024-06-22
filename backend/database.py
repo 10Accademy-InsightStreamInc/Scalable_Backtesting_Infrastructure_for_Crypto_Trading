ec4438dd019a67d97bac80b6deec353d4421dd69
dev-abuke <abubekershamil123@gmail.com>
2024-06-22 14:14:44 +0300

    add user schema

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("AWS_DATABASE_URL")
+DATABASE_URL = os.getenv("AWS_DATABASE_URL", "postgresql+psycopg2://group3:group3%40week9@g3.10academy.org/backtest")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 from datetime import date, datetime
 
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: Optional[str] = None
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class UserInDB(User):
+    hashed_password: str
+    
 class IndicatorBase(BaseModel):
     name: str
     symbol: str

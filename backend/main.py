c0c0dd95b8ccccaf93bd324d9bcfde41ebc9e752
dev-abuke <abubekershamil123@gmail.com>
2024-06-24 10:11:23 +0000

    feat: implement scene API get with id and refactore kafka consumer

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from typing import List
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.exc import IntegrityError
 import pandas as pd
-import yfinance as yf
 import threading
 
 from . import models, schemas, database
@@ -13,27 +13,15 @@ from backend.utils.backtest import run_backtest
 from backend.utils.init_data import initialize_data
 
 get_db = database.get_db
+
 consumer = get_kafka_consumer(SCENE_TOPIC)
+producer = get_kafka_producer()
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-def consume_scene_parameters():
-    for message in consumer:
-        scene_parameters = message.value
-        print(f"Received scene parameters: {scene_parameters}")
-        # Run the backtest and produce the results
-        # df = fetch_data(scene_parameters['start_date'], scene_parameters['end_date'])
-        # def download_data(ticker, start_date, end_date):
-        data = yf.download(scene_parameters.stock.symbol, start=scene_parameters['start_date'], end=scene_parameters['end_date'])
-        
-        metrics = run_backtest(scene_parameters, data)
-        producer = get_kafka_producer()
-        producer.send(RESULT_TOPIC, metrics)
-        producer.flush()
-
-@app.on_event("startup")
+@app.on_event("startup") # TODO update the code with lifespan dependency
 def start_kafka_consumer():
     threading.Thread(target=consume_scene_parameters).start()
 
@@ -43,11 +31,10 @@ def on_startup():
     initialize_data(db)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-producer = get_kafka_producer()
 
 @app.get('/health')
 def check_health():
-    return "API is healthy"
+    return "API is working and healthy"
 
 @app.post('/indicators/', response_model=schemas.Indicator)
 def create_indicator(indicator: schemas.IndicatorCreate, db: Session = Depends(get_db)):
@@ -77,28 +64,51 @@ def read_stocks(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
 
 @app.post('/scenes/', response_model=schemas.Scene)
 def create_scene(scene: schemas.SceneCreate, db: Session = Depends(get_db)):
-    db_scene = models.Scene(**scene.model_dump())
-    # check if the stock is already in the database
-    print("the scene is: ", db_scene.indicator)
-    if not db.query(models.Stock).filter(models.Stock.id == db_scene.stock_id).first():
-        raise HTTPException(status_code=400, detail="Stock ID does not exists")
-    if not db.query(models.Indicator).filter(models.Indicator.id == db_scene.indicator_id).first():
-        raise HTTPException(status_code=400, detail="Indicator ID does not exists")
-    db.add(db_scene)
+    try:
+        db_scene = models.Scene(**scene.dict())
+        db.add(db_scene)
+        db.commit()
+        db.refresh(db_scene)
+        print("Scene created successfully:", db_scene)
+
+        # Send scene parameters to Kafka
+        scene_parameters = {
+            'period': db_scene.period,
+            'indicator_name': db_scene.indicator.name,
+            'indicator_symbol': db_scene.indicator.symbol,
+            'stock_name': db_scene.stock.name,
+            'stock_symbol': db_scene.stock.symbol,
+            'start_date': db_scene.start_date.strftime('%Y-%m-%d'),
+            'end_date': db_scene.end_date.strftime('%Y-%m-%d')
+        }
+        print("Sending scene parameters to Kafka:", scene_parameters)
+        producer.send(SCENE_TOPIC, scene_parameters)
+        producer.flush()
+
+        return db_scene
+    except IntegrityError:
+        db.rollback()
+        existing_scene = db.query(models.Scene).filter(
+            models.Scene.start_date == scene.start_date,
+            models.Scene.end_date == scene.end_date,
+            models.Scene.indicator_id == scene.indicator_id
+        ).first()
+        return existing_scene
+
+@app.delete('/scenes/{scene_id}', response_model=schemas.Scene)
+def delete_scene(scene_id: int, db: Session = Depends(get_db)):
+    db_scene = db.query(models.Scene).options(joinedload(models.Scene.indicator), joinedload(models.Scene.stock)).filter(models.Scene.id == scene_id).first()
+    if db_scene is None:
+        raise HTTPException(status_code=404, detail="Scene not found")
+    db.delete(db_scene)
     db.commit()
-    db.refresh(db_scene)
+    return db_scene
 
-    # Send scene parameters to Kafka
-    scene_parameters = {
-        'period': db_scene.period,
-        'indicator_symbol': db_scene.indicator.symbol,
-        'stock_symbol': db_scene.stock.symbol,
-        'start_date': db_scene.start_date.strftime('%Y-%m-%d'),
-        'end_date': db_scene.end_date.strftime('%Y-%m-%d')
-    }
-    producer.send(SCENE_TOPIC, scene_parameters)
-    producer.flush()
-
+@app.get('/scenes/{scene_id}', response_model=schemas.Scene)
+def read_scene(scene_id: int, db: Session = Depends(get_db)):
+    db_scene = db.query(models.Scene).options(joinedload(models.Scene.indicator), joinedload(models.Scene.stock)).filter(models.Scene.id == scene_id).first()
+    if db_scene is None:
+        raise HTTPException(status_code=404, detail="Scene not found")
     return db_scene
 
 @app.get('/scenes/', response_model=List[schemas.Scene])
@@ -144,5 +154,39 @@ def read_backtest_results(skip: int = 0, limit: int = 10, db: Session = Depends(
 
 def fetch_data(start_date, end_date):
     # Replace this with actual data fetching logic
-    df = pd.read_csv('btc_usdt_candlestick.csv', index_col='timestamp', parse_dates=True)
+    df = pd.read_csv('data/binance_btc_usdt_candlestick.csv', index_col='timestamp', parse_dates=True)
     return df.loc[start_date:end_date]
+
+def fetch_existing_backtest(scene_parameters):
+    # Function to fetch existing backtest results from the database
+    db = next(get_db())
+    existing_scene = db.query(models.Scene).filter(
+        models.Scene.start_date == scene_parameters['start_date'],
+        models.Scene.end_date == scene_parameters['end_date'],
+        models.Scene.period == scene_parameters['period'],
+        # models.Scene.stock.symbol == scene_parameters['stock_symbol'],
+        # models.Scene.indicator.symbol == scene_parameters['indicator_symbol']
+    ).first()
+    if existing_scene:
+        existing_backtests = db.query(models.BacktestResult).filter(
+            models.BacktestResult.scene_id == existing_scene.id
+        ).all()
+        return existing_backtests
+    return None
+
+def consume_scene_parameters():
+    consumer = get_kafka_consumer(SCENE_TOPIC)
+    for message in consumer:
+        scene_parameters = message.value
+        print(f"Received scene parameters: {scene_parameters}")
+        existing_results = fetch_existing_backtest(scene_parameters)
+        if existing_results:
+            print(f"Using existing backtest results for parameters: {scene_parameters}")
+            continue
+        # Run the backtest if no existing results
+        df = fetch_data(scene_parameters['start_date'], scene_parameters['end_date'])
+        metrics = run_backtest(scene_parameters, df)
+        producer = get_kafka_producer()
+        print(f"Sending backtest results: {metrics}")
+        producer.send(RESULT_TOPIC, metrics)
+        producer.flush()
